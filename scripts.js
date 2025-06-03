9581ac19f95365ad457ca6bb005a61b63b7ea1f0
lipaosbk <felipe_udt@hotmail.com>
2025-06-02 22:00:53 -0300

    commit projeto instagram

---
+++ scripts.js
@@ -0,0 +1,17 @@
+
+
+let imagem = document.querySelector( ".troca-imagem")
+
+    function trocaimagem(){
+        if(imagem.style.opacity == 0){
+        imagem.style.opacity = 1 
+        } 
+        
+    else {
+            imagem.style.opacity = 0
+        }
+       
+}
+    setInterval(trocaimagem, 3000)
+
+console.log(imagem)
